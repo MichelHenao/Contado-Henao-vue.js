e71a3e9ee33c5d6098b680f2227371b0bfced7e8
MichelHenao <michelvaleria0510@gmail.com>
2023-05-25 21:13:47 -0500

    primer commit con ejercicio

---
+++ app.js
@@ -0,0 +1,22 @@
+        // Crea la instancia de Vue
+       const {createApp} = Vue;
+       
+       createApp({
+            data() {
+                return {
+                    contador: 0
+                };
+            },
+
+            methods:{
+                incrementar() {
+                    this.contador++;
+                },
+
+                decrementar(){
+                    if(this.contador > 0){
+                        this.contador--;
+                    }
+                }
+            }
+       }).mount("#app");
